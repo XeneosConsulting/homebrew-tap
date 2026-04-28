4204dd1be6a9f5bc13745537f07e8c3c8db2cbb1
xcg-automation <automation@xeneos.com>
2026-04-28 13:38:44 +0000

    Bump xcg to v2.0.0-beta.2

--- Formula/xcg.rb
+++ Formula/xcg.rb
@@ -3,11 +3,11 @@ require_relative "../custom_download_strategy.rb"
 class Xcg < Formula
   desc "xcg"
   homepage "https://software.xeneos.com/internal/cli"
-  version "v2.0.0-beta.1"
+  version "v2.0.0-beta.2"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/XeneosConsulting/xcg-cli/releases/download/v2.0.0-beta.1/xcg_v2.0.0-beta.1_macos-arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "39bcfcce20ecf073e173aff67ebb5d6562963f9dc1856bcf47a846bdbb3eafc8"
+    url "https://github.com/XeneosConsulting/xcg-cli/releases/download/v2.0.0-beta.2/xcg_v2.0.0-beta.2_macos-arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "1887bd5a03e59a82890ee1afe11aedac0d9e6e35cfee6738eee820b470d2bd73"
   end
 
   conflicts_with "xcg"
